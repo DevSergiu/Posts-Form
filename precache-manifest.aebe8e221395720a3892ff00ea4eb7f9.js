e710d746b7a21eda6eaaadba5fec520c0c91406e
Sergiu-19 <42917431+Sergiu-19@users.noreply.github.com>
2019-10-30 10:28:08 +0200

    Updates

---
+++ precache-manifest.aebe8e221395720a3892ff00ea4eb7f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61f35b2f61999384e11980385f09579",
+    "url": "/posts-from/index.html"
+  },
+  {
+    "revision": "c5d94a378675bd961a72",
+    "url": "/posts-from/static/css/main.928b1e2a.chunk.css"
+  },
+  {
+    "revision": "09e8057e9bba9d7919e7",
+    "url": "/posts-from/static/js/2.1e14a26f.chunk.js"
+  },
+  {
+    "revision": "c5d94a378675bd961a72",
+    "url": "/posts-from/static/js/main.6316b2fc.chunk.js"
+  },
+  {
+    "revision": "efc117289653fe99dfe8",
+    "url": "/posts-from/static/js/runtime-main.fb4da4ed.js"
+  }
+]);
